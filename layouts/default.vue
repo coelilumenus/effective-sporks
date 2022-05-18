c286c63f5e6d35fa8b9427828569fc7845179270
coelilumenus <war.nub@yandex.ru>
2022-05-18 21:02:21 +0300

    init

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="h-full flex gap-x-2">
+    <side-bar />
+
+    <div class="contaner p-4">
+      <nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+import SideBar from '@/components/SideBar.vue'
+
+export default {
+  components: {
+    SideBar,
+  },
+}
+</script>
