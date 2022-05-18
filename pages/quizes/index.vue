6be999e124ed51c39fe2cb68fa3ad8d10d3f8ea3
coelilumenus <war.nub@yandex.ru>
2022-05-18 21:54:19 +0300

    init quiz tabs

--- pages/quizes/index.vue
+++ pages/quizes/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>quizes index</div>
+  <div>
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="Параметры" name="settings">Параметры</el-tab-pane>
+      <el-tab-pane label="Вопросы" name="questions">Вопросы</el-tab-pane>
+      <el-tab-pane label="Логика" name="logics">Логика</el-tab-pane>
+      <el-tab-pane label="Условия" name="conditions">Условия</el-tab-pane>
+      <el-tab-pane label="Респонденты" name="respondents">Респонденты</el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 
 <script>
@@ -8,6 +16,12 @@ export default {
 
   meta: {
     name: 'Опросы',
+  },
+
+  data() {
+    return {
+      activeName: 'respondents',
+    }
   },
 }
 </script>
