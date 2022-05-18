33cda7dfef7955baf2d236037b46255ca240c0de
coelilumenus <war.nub@yandex.ru>
2022-05-18 21:04:39 +0300

    create sidebar

---
+++ components/SideBar.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="flex flex-col items-center max-w-min min-w-max h-screen border-r border-gray-400 p-2">
+    <nuxt-link
+      to="/quizes"
+      exact
+      active-class="text-blue-400"
+      class="flex flex-col items-center rounded-md hover:text-blue-400 active:bg-blue-200 h-20 text-gray-500 font-medium max-w-min"
+    >
+      <IconDescription class="fill-current" />
+      <span class="ml-1 font-medium whitespace-nowrap text-xs"> Опросы </span>
+    </nuxt-link>
+
+    <nuxt-link
+      to="/users"
+      active-class="text-blue-400"
+      class="flex flex-col items-center rounded-md hover:text-blue-400 active:bg-blue-200 h-20 text-gray-500 font-medium max-w-min"
+    >
+      <IconGroup class="fill-current" />
+      <span class="ml-1 font-medium whitespace-nowrap text-xs"> Пользователи </span>
+    </nuxt-link>
+
+    <nuxt-link
+      to="/blacklists"
+      active-class="text-blue-400"
+      class="flex flex-col items-center rounded-md hover:text-blue-400 active:bg-blue-200 h-20 text-gray-500 font-medium max-w-min"
+    >
+      <IconFlag class="fill-current" />
+      <span class="ml-1 font-medium whitespace-nowrap text-xs"> Чёрные списки </span>
+    </nuxt-link>
+
+    <nuxt-link
+      to="/callcenter"
+      active-class="text-blue-400"
+      class="flex flex-col items-center rounded-md hover:text-blue-400 active:bg-blue-200 h-20 text-gray-500 font-medium max-w-min"
+    >
+      <IconCall class="fill-current" />
+      <span class="ml-1 font-medium whitespace-nowrap text-xs"> Колл-центр </span>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+import IconDescription from '@/static/description.svg'
+import IconCall from '@/static/call.svg'
+import IconFlag from '@/static/flag.svg'
+import IconGroup from '@/static/group.svg'
+
+export default {
+  name: 'SideBar',
+
+  components: {
+    IconDescription,
+    IconCall,
+    IconFlag,
+    IconGroup
+  },
+}
+</script>
