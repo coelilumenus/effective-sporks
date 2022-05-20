37e23dd6596b49c8f70c08a6dbc86fb6aa2eafb4
Valentine <war.nub@yandex.ru>
2022-05-21 00:33:51 +0300

    implement ConditionAge

--- components/quizes/respondents/ConditionsComponents/ConditionAge.vue
+++ components/quizes/respondents/ConditionsComponents/ConditionAge.vue
@@ -1,3 +1,91 @@
 <template>
-    <div></div>
+  <div>
+    <condition-age-range
+      v-for="(range, i) in value.ranges"
+      :key="'range' + i"
+      v-model="value.ranges[i]"
+      :index="i + 1"
+    />
+
+    <div class="flex w-full justify-between items-center">
+      <div class="w-full max-w-xs"></div>
+
+      <div class="flex w-full relative top-8">
+        <el-button
+          class="absolute"
+          type="primary"
+          plain
+          icon="el-icon-plus"
+          @click="onAddRange"
+        >
+          Добавить диапазон
+        </el-button>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import ConditionAgeRange from './ConditionAgeRange.vue'
+
+export default {
+  name: 'ConditionAge',
+
+  components: {
+    ConditionAgeRange,
+  },
+
+  model: {
+    prop: 'value',
+    event: 'change',
+  },
+
+  props: {
+    value: {
+      type: Object,
+    },
+  },
+
+  data() {
+    return {
+      defaultData: {
+        ranges: [
+          {
+            start: 0,
+            end: 0,
+          },
+        ],
+      },
+    }
+  },
+
+  computed: {
+    isDataCorrect() {
+      if (!this.value.data) return false
+
+      return Object.keys(this.value.data).every((property) => {
+        return this.defaultData.hasOwnProperty(property)
+      })
+    },
+  },
+
+  created() {
+    if (!this.isValueCorrect) {
+      this.initDefaultData()
+    }
+  },
+
+  methods: {
+    initDefaultData() {
+      this.$emit('initDefaultData', this.defaultData)
+    },
+
+    onAddRange() {
+      this.value.ranges.push({
+        start: 0,
+        end: 0,
+      })
+    },
+  },
+}
+</script>
---
+++ components/quizes/respondents/ConditionsComponents/ConditionAgeRange.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex w-full justify-between items-center mb-8">
+    <div class="w-full max-w-xs"><span class="bg-blue-200 py-1 px-2 rounded mr-2" v-if="index > 1">или</span> Диапазон {{ index }}</div>
+
+    <div class="flex gap-x-4 w-full">
+      <div class="flex items-center gap-x-2">
+        от <el-input-number v-model="value.start" />
+      </div>
+      <div class="flex items-center gap-x-2">
+        до <el-input-number v-model="value.end" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ConditionAgeRange',
+
+  props: {
+    value: {
+      type: Object,
+    },
+
+    index: {
+      type: Number,
+    },
+  },
+}
+</script>
