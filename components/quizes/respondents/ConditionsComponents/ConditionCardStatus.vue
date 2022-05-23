7175c0cbedba8965a6adb59b51221e32344cad6f
Valentine <war.nub@yandex.ru>
2022-05-23 22:57:26 +0300

    init cardStatus

--- components/quizes/respondents/ConditionsComponents/ConditionCardStatus.vue
+++ components/quizes/respondents/ConditionsComponents/ConditionCardStatus.vue
@@ -1,7 +1,88 @@
 <template>
-  <div>cardstatus</div>
+  <div>
+    <condition-card-status-select
+      v-for="(status, i) in value.statuses"
+      :key="'status' + i"
+      :value="value.statuses[i]"
+      :index="i + 1"
+      @changeStatus="changeStatus"
+    />
+
+    <div class="flex w-full justify-between items-center">
+      <div class="w-full max-w-xs"></div>
+
+      <div class="flex w-full relative top-8">
+        <el-button
+          class="absolute"
+          type="primary"
+          plain
+          icon="el-icon-plus"
+          @click="onAddStatus"
+        >
+          Добавить статус
+        </el-button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import ConditionCardStatusSelect from './ConditionCardStatusSelect.vue'
+
+export default {
+  name: 'ConditionCardStatus',
+
+  components: {
+    ConditionCardStatusSelect,
+  },
+
+  model: {
+    prop: 'value',
+    event: 'change',
+  },
+
+  props: {
+    value: {
+      type: Object,
+    },
+  },
+
+  data() {
+    return {
+      defaultData: {
+        statuses: ['Gold']
+      },
+    }
+  },
+
+  computed: {
+    isDataCorrect() {
+      if (!this.value.data) return false
+
+      return Object.keys(this.value.data).every((property) => {
+        return this.defaultData.hasOwnProperty(property)
+      })
+    },
+  },
+
+  created() {
+    if (!this.isValueCorrect) {
+      this.initDefaultData()
+    }
+  },
+
+  methods: {
+    initDefaultData() {
+      this.$emit('initDefaultData', this.defaultData)
+    },
+
+    onAddStatus() {
+      this.value.statuses.push('Gold')
+    },
+    
+    changeStatus(status, index) {
+      this.$set(this.value.statuses, index, status)
+    }
+  },
+}
 </script>
---
+++ components/quizes/respondents/ConditionsComponents/ConditionCardStatusSelect.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="flex w-full justify-between items-center mb-8">
+    <div class="w-full max-w-xs"><span class="bg-blue-200 py-1 px-2 rounded mr-2" v-if="index > 1">или</span> Статус {{ index }}</div>
+
+    <div class="flex gap-x-4 w-full">
+      <div class="flex items-center gap-x-2">
+        <el-select :value="value" @change="changeStatus">
+          <el-option
+            v-for="(option, i) in options"
+            :key="option + i"
+            :value="option"
+            :label="option"
+          />
+        </el-select>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ConditionCardStatusSelect',
+  
+  data() {
+    return {
+      options: ['Gold', 'Silver', 'Bronze'],
+    }
+  },
+
+  props: {
+    value: {
+      type: String,
+    },
+
+    index: {
+      type: Number,
+    },
+  },
+  
+  methods: {
+    changeStatus(status) {
+      this.$emit('changeStatus', status, this.index - 1)
+    }
+  }
+}
+</script>
