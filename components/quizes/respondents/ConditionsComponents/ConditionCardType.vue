3fddacc34512f269af2b8c80c02a6afe2960b23d
Valentine <war.nub@yandex.ru>
2022-05-23 22:57:33 +0300

    init cardType

--- components/quizes/respondents/ConditionsComponents/ConditionCardType.vue
+++ components/quizes/respondents/ConditionsComponents/ConditionCardType.vue
@@ -1,7 +1,88 @@
 <template>
-  <div>cardtype</div>
+  <div>
+    <condition-card-type-select
+      v-for="(type, i) in value.types"
+      :key="'type' + i"
+      :value="value.types[i]"
+      :index="i + 1"
+      @changeType="changeType"
+    />
+
+    <div class="flex w-full justify-between items-center">
+      <div class="w-full max-w-xs"></div>
+
+      <div class="flex w-full relative top-8">
+        <el-button
+          class="absolute"
+          type="primary"
+          plain
+          icon="el-icon-plus"
+          @click="onAddType"
+        >
+          Добавить тип
+        </el-button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import ConditionCardTypeSelect from './ConditionCardTypeSelect.vue'
+
+export default {
+  name: 'ConditionCardStatus',
+
+  components: {
+    ConditionCardTypeSelect,
+  },
+
+  model: {
+    prop: 'value',
+    event: 'change',
+  },
+
+  props: {
+    value: {
+      type: Object,
+    },
+  },
+
+  data() {
+    return {
+      defaultData: {
+        types: ['Активна']
+      },
+    }
+  },
+
+  computed: {
+    isDataCorrect() {
+      if (!this.value.data) return false
+
+      return Object.keys(this.value.data).every((property) => {
+        return this.defaultData.hasOwnProperty(property)
+      })
+    },
+  },
+
+  created() {
+    if (!this.isValueCorrect) {
+      this.initDefaultData()
+    }
+  },
+
+  methods: {
+    initDefaultData() {
+      this.$emit('initDefaultData', this.defaultData)
+    },
+
+    onAddType() {
+      this.value.types.push('Активна')
+    },
+    
+    changeType(status, index) {
+      this.$set(this.value.types, index, status)
+    }
+  },
+}
 </script>
---
+++ components/quizes/respondents/ConditionsComponents/ConditionCardTypeSelect.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="flex w-full justify-between items-center mb-8">
+    <div class="w-full max-w-xs"><span class="bg-blue-200 py-1 px-2 rounded mr-2" v-if="index > 1">или</span> Тип {{ index }}</div>
+
+    <div class="flex gap-x-4 w-full">
+      <div class="flex items-center gap-x-2">
+        <el-select :value="value" @change="changeType">
+          <el-option
+            v-for="(option, i) in options"
+            :key="option + i"
+            :value="option"
+            :label="option"
+          />
+        </el-select>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ConditionCardTypeSelect',
+  
+  data() {
+    return {
+      options: ['Активна', 'Не активна'],
+    }
+  },
+
+  props: {
+    value: {
+      type: String,
+    },
+
+    index: {
+      type: Number,
+    },
+  },
+  
+  methods: {
+    changeType(type) {
+      this.$emit('changeType', type, this.index - 1)
+    }
+  }
+}
+</script>
